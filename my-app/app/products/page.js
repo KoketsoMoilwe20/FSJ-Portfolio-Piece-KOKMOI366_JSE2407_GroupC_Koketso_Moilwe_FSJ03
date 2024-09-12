b018aec75e066600dcf0a57d93b48ded3aa8066d
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-09-12 13:19:31 +0200

    Create products page with dynamic product list and pagination

---
+++ my-app/app/products/page.js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import ProductList from '../components/ProductList';
+import styles from "../styles/products.module.css";
+
+async function getProducts() {
+    const res = await fetch('https://next-ecommerce-api.vercel.app/products?limit=20', { cache: 'no-store' });
+    if (!res.ok) {
+      throw new Error('Failed to fetch products');
+    }
+    const data = await res.json();
+    console.log(data); // Check if data is structured correctly
+    return data;
+  }
+  
+
+export default async function ProductsPage() {
+  const data = await getProducts();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Our Products</h1>
+
+      {/* Back Button */}
+      <Link href="/" classsName={styles.backButton}>
+        <button className={styles.backBtn}>← Back to Home</button>
+      </Link>
+      <ProductList initialProducts={data} />
+    </div>
+  );
+}
