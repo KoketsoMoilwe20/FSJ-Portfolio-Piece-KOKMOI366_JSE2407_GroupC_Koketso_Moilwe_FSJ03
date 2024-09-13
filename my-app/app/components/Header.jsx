aec734ab5c40335b280f0e168ec87dbe68288ad6
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-09-13 14:48:50 +0200

    Create Header.jsx

---
+++ my-app/app/components/Header.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css' // Import the CSS module
+
+export default function Header() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Link href="/" className={styles.logo}>
+          {/* <img src="/online-shop.png" alt="Shop Logo" className={styles.logoImage} /> */}
+          <span className={styles.logoText}>E-Commerce Store</span>
+        </Link>
+
+        <button onClick={toggleNavbar} className={styles.menuButton}>
+          &#9776;
+        </button>
+
+        <nav className={`${styles.nav} ${showNavbar ? styles.navVisible : styles.navHidden}`}>
+          <Link href="/wishlist" className={styles.navLink}>
+            Wishlist
+          </Link>
+          <Link href="/cart" className={styles.navLink}>
+            Cart
+          </Link>
+          <Link href="/login" className={styles.navLink}>
+            Login
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
