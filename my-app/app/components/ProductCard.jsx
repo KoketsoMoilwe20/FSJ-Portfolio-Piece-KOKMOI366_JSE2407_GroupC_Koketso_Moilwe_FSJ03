18140560486b4a110f75b591b86e32b2929d42c5
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-09-14 14:01:42 +0200

    Add: JSDoc

--- my-app/app/components/ProductCard.jsx
+++ my-app/app/components/ProductCard.jsx
@@ -1,12 +1,27 @@
 import Link from 'next/link';
 import ProductCarousel from './ProductCarousel';
-import styles from '../styles/ProductCard.module.css'; // Import the CSS module
+import styles from '../styles/ProductCard.module.css'; 
+
+/**
+ *  ProductCard component that displays a product's image, title, price, and category.
+ * If the product has multiple images, it displays a carousel of images.
+ * 
+ * @param {Object} props - The component props. 
+ * @param {Object} props.product - The product data to display.
+ * @param {string} props.product.id - The unique identifier for the product.
+ * @param {string} props.product.title - The title of the product.
+ * @param {number} props.product.price - The price of the product.
+ * @param {string} props.product.category - The category of the product.
+ * @param {Array<string>} props.product.images - An array of image URLs for the product.
+ * 
+ * @returns {JSX.Element} The rendered product card component.
+ */
 
 export default function ProductCard({ product }) {
   return (
     <div className={styles.productCard}>
       <div className={styles.imageContainer}>
-        {/* Only wrap title, price, and category in the Link, not the carousel */}
+        
         {product.images && product.images.length > 1 ? (
           <ProductCarousel images={product.images} />
         ) : (
