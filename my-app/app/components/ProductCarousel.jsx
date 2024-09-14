25b6c95f11cdc85d3192db2b8c289e5effcbbf50
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-09-14 14:07:59 +0200

    Add: JSDocs and comments

--- my-app/app/components/ProductCarousel.jsx
+++ my-app/app/components/ProductCarousel.jsx
@@ -1,18 +1,32 @@
-'use client'; // This tells Next.js this is a client-side component
+'use client'; // This is a client-side component
 
 import { useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
-import styles from '../styles/productCarousel.module.css'; // Import the CSS module
+import styles from '../styles/productCarousel.module.css'; 
+
+/**
+ * ProductCarousel component for displaying a carousel of product images.
+ * Allows navigation between images using arrow buttons.
+ * 
+ * @param {Object} props - The component props.
+ * @param {Array<string>} props.images - An array of image URLs to display in the carousel.
+ * 
+ * @returns {JSX.Element} The rendered product carousel component.
+ */
 
 export default function ProductCarousel({ images }) {
+  // State to keep track of the currently displayed image index
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+
+  // Navigate to the next image in the carousel.
   const handleNext = () => {
     if (currentImageIndex < images.length - 1) {
       setCurrentImageIndex(currentImageIndex + 1);
     }
   };
 
+  // Navigate to the previous image in the carousel.
   const handlePrev = () => {
     if (currentImageIndex > 0) {
       setCurrentImageIndex(currentImageIndex - 1);
